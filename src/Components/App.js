8413039fdc82a1b8bbff05d3a39f54b68a1c0108
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-01 22:46:26 +0100

    Adds passing test for adding a bucket list item

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
 
@@ -9,11 +9,26 @@ class App extends Component {
     this.state = { items: [] };
   }
 
+  addItem = () => {
+    const { items } = this.state;
+
+    const ids = this.state.items.map(item => item.id);
+
+    const max_id = ids.length > 0 ? Math.max(...ids) : 0;
+
+    items.push({ id: max_id+1 })
+
+    this.setState({ items });
+  }
+
   render() {
 
     return (
       <div className="App">
-      
+      <Button 
+        onClick={this.addItem} 
+        className="add-item"
+        >Add Item</Button>
       </div>
   );
   };
--- src/Components/App.test.js
+++ src/Components/App.test.js
@@ -16,4 +16,9 @@ describe('App', () => {
   it('is initialized with an empty array', () => {
     expect(wrapper.state().items).toEqual([]);
   });
+
+  it('adds a bucket list item to the items array', () => {
+    wrapper.find('.add-item').simulate('click');
+    expect(wrapper.state().items).toEqual([{ id: 1}]);
+  });
 });
